c4817435805ce85e8fed912d79da324ce151ff1e
redbanny <giorgiavakov@yandex.ru>
2023-07-08 23:56:23 +0300

    Создание класса 'StackExtensions'

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -1,4 +1,5 @@
 ﻿var s = new Stack("a", "b", "c");
+s.Merge(new Stack("1", "2", "3"));
 // size = 3, Top = 'c'
 Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
 var deleted = s.Pop();
@@ -11,5 +12,5 @@ s.Pop();
 s.Pop();
 s.Pop();
 // size = 0, Top = null
-Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
+Console.WriteLine($"size = {s.Size}, Top = {(s.Top ?? "null")}");
 s.Pop();
---
+++ Homework_5/StackExtensions.cs
@@ -0,0 +1,8 @@
+public static class StackExtensions
+{
+    public static void Merge(this Stack s1, Stack s2)
+    {
+        for (int i = s2.Size; i > 0; i--)
+            s1.Add(s2.Pop());
+    }
+}
